ff5299d60d74e4e31e3b259aa20d3f5a79611064
Andrey Matyushin <andrey.matyushin05@gmail.com>
2025-02-19 17:20:09 +0300

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML with CMake");
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        window.clear(sf::Color::Black);
+        window.clear();
+        window.draw(shape);
         window.display();
     }
-
-    return 0;
 }
