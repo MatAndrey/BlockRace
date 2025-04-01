50b54a6a9b354e21217e7c5f7de65e46613adf87
Andrey Matyushin <andrey.matyushin05@gmail.com>
2025-04-01 18:31:05 +0300

    fixed missing RotationBlock.cpp

---
+++ src/Blocks/RotationBlock.cpp
@@ -0,0 +1,83 @@
+#include "RotationBlock.hpp"
+
+RotationBlock::RotationBlock(sf::Vector2f _pos, sf::RenderWindow* window, sf::Angle _dir) :
+    Block(_pos, { 120, 30 }, window), direction(_dir), shape1({ 20, 30 }), shape2({ 25, 30 }), shape3({ 75, 30 })
+{
+    shape1.setFillColor(sf::Color(44, 71, 122));
+
+    shape2.setFillColor(sf::Color(44, 71, 122));
+    shape2.setPosition(sf::Vector2f(20, 5));
+
+    shape3.setFillColor(sf::Color(44, 71, 122));
+    shape3.setPosition(sf::Vector2f(45, 0));
+
+    if (direction == sf::degrees(0)) {
+        text.setString(L"Прямо");
+    }
+    else if (direction > sf::degrees(0)) {
+        text.setString(L"Направо");
+    }
+    else {
+        text.setString(L"Налево");
+    }
+    
+    text.setCharacterSize(14);
+    text.setPosition(sf::Vector2f{ 5, 8 });
+
+    std::vector<sf::Vector2f> vertexes{
+        {0, 0}, {20, 0}, {20, 5}, {45, 5}, {45, 0}, {120, 0}, {120, 30},
+        {45, 30}, {45, 35}, {20, 35}, {20, 30}, {0, 30}, {0, 0}
+    };
+    outline.resize(vertexes.size());
+    for (int i = 0; i < vertexes.size(); i++) {
+        outline[i].position = vertexes[i];
+        outline[i].color = sf::Color::White;
+    }
+}
+
+RotationBlock::~RotationBlock()
+{
+}
+
+float RotationBlock::getAngle()
+{
+    return direction.asDegrees();
+}
+
+RotationBlock* RotationBlock::clone()
+{
+    return new RotationBlock(pos, window, direction);
+}
+
+void RotationBlock::render()
+{
+    sf::Transform transform;
+    transform.translate(pos);
+
+    window->draw(shape1, transform);
+    window->draw(shape2, transform);
+    window->draw(shape3, transform);
+    window->draw(outline, transform);
+
+    window->draw(text, transform);
+}
+
+Block* RotationBlock::update(Car& car)
+{
+    car.setDirection(direction);
+    if (nextBlock) {
+        return nextBlock;
+    }
+    else {
+        return nullptr;
+    }
+}
+
+std::string RotationBlock::name()
+{
+    return "RotationBlock";
+}
+
+RotationBlock::RotationBlock(sf::Vector2f _pos, sf::RenderWindow* window, sf::Angle _dir)
+{
+}
